5c65d7cae2ea98d311f405408cfee2d08374a76a
Kiralyroli <kiralyroli96@gmail.com>
2025-01-31 22:04:51 +0100

    Home visszajelzések rész elkészítése

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,15 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home_page')]
     public function index(): Response
     {
-        return $this->render('home/home.html.twig');
+        $filePath = $this->getParameter('kernel.project_dir') . '/data/testimonials.json';
+        if (!file_exists($filePath)) {
+            throw $this->createNotFoundException('A testimonials.json fájl nem található!');
+        }
+        $jsonData = file_get_contents($filePath);
+        $testimonials = json_decode($jsonData, true);
+
+        return $this->render('home/home.html.twig', [
+            'testimonials' => $testimonials,
+        ]);
     }
 }
